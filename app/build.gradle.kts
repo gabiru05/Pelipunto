9e0db7a16f98d6e36f3c51b18d40153e74e005b0
Adolfo Lopez <lopezavendanoa@gmail.com>
2025-07-24 14:23:37 -0500

    Actualización: ADD "Barra de navegación" por ahora esta: - Home - Ajustes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material3:material3:1.1.2") // O la última versión estable que uses
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
